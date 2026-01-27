4996d9efd24fa62a30059eae9e848cb1924f3643
Karol Roczniak <karolroczniak23@gmail.com>
2026-01-27 12:29:13 +0100

    astro name fix

---
+++ .wrangler/tmp/bundle-JEhvux/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/home/karol/Code/19-18-8-103/dist/_worker.js/index.js";
+				import * as __MIDDLEWARE_0__ from "/home/karol/.cache/pnpm/dlx/dc886f2388b7ffeed8b389bce30b5947d36c8de36b4f8263e81f755685b9d2de/19bfa3d1333-41be/node_modules/.pnpm/wrangler@4.60.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/home/karol/.cache/pnpm/dlx/dc886f2388b7ffeed8b389bce30b5947d36c8de36b4f8263e81f755685b9d2de/19bfa3d1333-41be/node_modules/.pnpm/wrangler@4.60.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/home/karol/Code/19-18-8-103/dist/_worker.js/index.js";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
